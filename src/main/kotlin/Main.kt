05ee0e4df9b6dc3a2bc806a01770207064f52e55
Akash Jain <ajcodes.tech@gmail.com>
2025-03-13 19:12:23 +0000

    stream message

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,17 @@
 import java.net.ServerSocket;
 
 fun main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
     println("Logs from your program will appear here!")
     var serverSocket = ServerSocket(4221)
-    //
-    // // Since the tester restarts your program quite often, setting SO_REUSEADDR
-    // // ensures that we don't run into 'Address already in use' errors
-     serverSocket.reuseAddress = true
-    //
-     serverSocket.accept() // Wait for connection from client.
-     println("accepted new connection")
+    serverSocket.reuseAddress = true
+    serverSocket.receiveBufferSize
+
+    // wait for clients
+    val clientSocket = serverSocket.accept()
+
+    // get output stream
+    val buffer = clientSocket.getOutputStream()
+    buffer.write("HTTP/1.1 200 OK\\r\\n\\r\\n\n".toByteArray())
+    buffer.flush()
+    buffer.close()
 }
