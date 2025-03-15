9262eef062cb188fb7d35bf7345ec7102f1081a1
Akash Jain <ajcodes.tech@gmail.com>
2025-03-15 16:45:42 +0000

    added user-agent header

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,22 @@
-import java.net.ServerSocket;
+import java.net.ServerSocket
+
+
+const val CRLF = "\r\n"
+
+object HttpCodes {
+    const val HTTP_200 =  "HTTP/1.1 200 OK"
+    const val HTTP_404 =  "HTTP/1.1 404 Not Found"
+}
+
+enum class ContentType(val value: String) {
+    TEXT_PLAIN("text/plain")
+}
+
+object HttpHeader {
+    fun contentLength(str: String) = "Content-Length: ${str.length}"
+    fun contentType(contentType: ContentType) = "Content-Type: ${contentType.value}"
+}
+
 
 fun main() {
     println("Logs from your program will appear here!")
@@ -14,18 +32,37 @@ fun main() {
     inputStream.bufferedReader().use {
         val line = it.readLine()
         val splitline = line.split(' ')
-
         val path = splitline[1]
+        it.readLine()
+        val userAgent = it.readLine()
         val response = when {
-            path == "/" -> "HTTP/1.1 200 OK\r\n\r\n"
+            path == "/" -> "${HttpCodes.HTTP_200}$CRLF$CRLF"
             path.startsWith("/echo/") -> {
                 val str = path.substringAfter("/echo/")
-                "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${str.length}\r\n\r\n${str}"
+                buildString {
+                    requestBody(str)
+                }
             }
-            else -> "HTTP/1.1 404 Not Found\r\n\r\n"
+            path.startsWith("/user-agent") -> {
+                val str = userAgent.split(": ")[1]
+                buildString {
+                    requestBody(str)
+                }
+            }
+            else -> "${HttpCodes.HTTP_404}$CRLF$CRLF"
         }
         outputStream.write(response.toByteArray())
         outputStream.flush()
         outputStream.close()
     }
+}
+
+private fun StringBuilder.requestBody(str: String) {
+    // Request Line
+    append(HttpCodes.HTTP_200).append(CRLF)
+    // Headers
+    append(HttpHeader.contentType(ContentType.TEXT_PLAIN)).append(CRLF)
+    append(HttpHeader.contentLength(str)).append(CRLF).append(CRLF)
+    // Request Body
+    append(str)
 }
