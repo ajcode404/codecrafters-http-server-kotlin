1c870b32b00b5af747be04d588f41c9b82baaf1c
Akash Jain <ajcodes.tech@gmail.com>
2025-03-24 17:49:53 +0000

    Fix attempt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,7 +7,13 @@ fun main() {
     serverSocket.receiveBufferSize
     // wait for clients
     val clientSocket = serverSocket.accept()
-    RequestHandler(clientSocket).handle()
+    val handler = RequestHandler(clientSocket)
+    val buffer = clientSocket.outputStream
+    val response = handler.handle()
+    buffer.write(response.toByteArray())
+    buffer.flush()
+    buffer.close()
+    println(response)
     // get output stream
 //    val buffer = clientSocket.getOutputStream()
 //    val response = "HTTP/1.1 200 OK\r\n\r\n"
